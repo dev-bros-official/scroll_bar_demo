7c5c43ed09c4398da2a0b4ffccfedf9614718d9b
mrgnhnt96 <mrgnhnt96@gmail.com>
2020-07-22 05:40:10 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ScrollController controller;
+
+  @override
+  void initState() {
+    controller = ScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Scrollbar(
+        controller: controller,
+        child: ListView.builder(
+          controller: controller,
+          itemCount: 50,
+          itemBuilder: (context, i) {
+            return Container(
+              height: 50,
+              margin: const EdgeInsets.all(16),
+              alignment: Alignment.center,
+              child: Text('$i'),
+              color: Colors.red,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
